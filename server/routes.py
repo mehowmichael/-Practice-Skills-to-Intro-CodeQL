fcef1e3ee7d57e950a2096a06b68a2da70758564
Michael Cyrwus <mcyrwus2005@gmail.com>
2025-06-10 20:52:20 -0500

    Updating insecure code to routes.py
    Injecting routes.py with bad code.

--- server/routes.py
+++ server/routes.py
@@ -13,13 +13,13 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + %s", name
+            "SELECT * FROM books WHERE name LIKE 's%" + name + %s", name
         )
         books = [Book(*row) for row in cursor]
 
     elif author:
         cursor.execute(
-            "SELECT * FROM books WHERE author LIKE '%" + author + %s", author
+            "SELECT * FROM books WHERE author LIKE 's%" + author + %s", author
         )
         books = [Book(*row) for row in cursor]
 
